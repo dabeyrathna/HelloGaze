00d4e73005c5b29c592509fd5bea7296c4352aa4
dilanga198 <dilanga198@gmail.com>
2018-06-09 05:13:42 -0500

    Add project files.

---
+++ FormsSamples/GazeAwareForms/GazeAwareForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace GazeAwareForms
+{
+    partial class GazeAwareForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.behaviorMap1 = new EyeXFramework.Forms.BehaviorMap(this.components);
+            this.btnSaveImage = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCoords = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(143, 97);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1182, 591);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // btnSaveImage
+            // 
+            this.btnSaveImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveImage.Location = new System.Drawing.Point(582, 713);
+            this.btnSaveImage.Name = "btnSaveImage";
+            this.btnSaveImage.Size = new System.Drawing.Size(274, 67);
+            this.btnSaveImage.TabIndex = 8;
+            this.btnSaveImage.Text = "Save the drawing";
+            this.btnSaveImage.UseVisualStyleBackColor = true;
+            this.btnSaveImage.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(486, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 39);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "v";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(486, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 39);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "vb";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCoords
+            // 
+            this.lblCoords.AutoSize = true;
+            this.lblCoords.Location = new System.Drawing.Point(1118, 13);
+            this.lblCoords.Name = "lblCoords";
+            this.lblCoords.Size = new System.Drawing.Size(11, 17);
+            this.lblCoords.TabIndex = 11;
+            this.lblCoords.Text = "l";
+            // 
+            // GazeAwareForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1490, 802);
+            this.Controls.Add(this.lblCoords);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSaveImage);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "GazeAwareForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gaze-Aware Windows Forms Sample";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private EyeXFramework.Forms.BehaviorMap behaviorMap1;
+        private System.Windows.Forms.Button btnSaveImage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCoords;
+    }
+}
+
---
+++ FormsSamples/GazeAwareForms/GazeAwareForm.cs
@@ -0,0 +1,138 @@
+﻿//-----------------------------------------------------------------------
+// Copyright 2014 Tobii Technology AB. All rights reserved.
+//-----------------------------------------------------------------------
+
+using EyeXFramework;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace GazeAwareForms
+{
+    public partial class GazeAwareForm : Form
+    {
+        Graphics g;
+        bool startPaint = false;
+        int? initX = null;
+        int? initY = null;
+        Pen p;
+        int count = 0;
+
+        public GazeAwareForm()
+        {
+            InitializeComponent();
+
+            // Add eye-gaze interaction behaviors to the panels on the form.
+            // The panels should display a border when the user's gaze are on them.
+            // Note that panel4 is nested inside panel2. This means that any time 
+            // panel2 has the user's gaze, panel4 will too.
+            Program.EyeXHost.Connect(behaviorMap1);
+            behaviorMap1.Add(panel1, new GazeAwareBehavior(OnGaze));
+            bmp = new Bitmap(panel1.ClientSize.Width, panel1.ClientSize.Height);
+            p = new Pen(Color.Green, 3);
+
+
+            //var stream = Program.EyeXHost.CreateEyePositionDataStream();
+
+            //stream.Next += (s, e) => MessageBox.Show("dsdasd", "sfsdfdsfds");
+
+
+        }
+
+        Bitmap bmp;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            g = Graphics.FromImage(bmp);
+            g.DrawLine(new Pen(Color.Blue, 10), 700, 200, 100, 300);
+            
+        }
+
+ 
+
+        private void OnGaze(object sender, GazeAwareEventArgs e)
+        {
+            var panel = sender as Panel;
+            if (panel != null)
+            {
+                panel.BorderStyle = (e.HasGaze) ? BorderStyle.FixedSingle : BorderStyle.None;
+
+                if (!e.HasGaze)
+                {
+                    runOnOutOfForcus();
+                }
+                else {
+                    runOnForcus();
+                }
+            }
+        }
+
+        private void runOnForcus() {
+            label2.Visible = false;
+            label1.Visible = true;
+            label1.Text = "Great You are on focus..";
+            label1.ForeColor = Color.Green;
+        }
+
+        private void runOnOutOfForcus()
+        {
+            label2.Visible = true;
+            label1.Visible = false;
+            label2.Text = "Hei.. focus on the drawing area... ";
+            label2.ForeColor = Color.Red;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            startPaint = true;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (startPaint)
+            {
+                using (g = Graphics.FromImage(bmp))
+                {
+                    
+                    g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+                    //p = new Pen(Color.Green, 3);
+                    if (label2.Visible)
+                    {
+                        p.Color = Color.Yellow;
+                    }
+                    else {
+                        p.Color = Color.Green;
+                    }
+                   
+                    g.DrawLine(p, new Point(initX ?? e.X, initY ?? e.Y), new Point(e.X, e.Y));
+                    
+                    initX = e.X;
+                    initY = e.Y;
+                }
+                panel1.Invalidate();
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            startPaint = false;
+            initX = null;
+            initY = null;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(bmp, Point.Empty);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bmp.Save("D://savedItems//OutFile " + count+".jpg", ImageFormat.Jpeg);
+            count++;
+        }
+
+    }
+}
